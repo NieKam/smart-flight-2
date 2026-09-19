1f5ff37ae665c522c42eddd02f26e236271e1b5c
Kamil <kamil.niezrecki@gmail.com>
2026-09-19 21:42:18 +0000

    fix: restore GNSS screen layout imports

--- app/src/main/java/kniezrec/com/flightinfo/ui/gnss/GnssStatusScreen.kt
+++ app/src/main/java/kniezrec/com/flightinfo/ui/gnss/GnssStatusScreen.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.widthIn
