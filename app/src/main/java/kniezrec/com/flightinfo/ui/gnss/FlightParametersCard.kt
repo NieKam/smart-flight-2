293eb12ef22c5147ebfc3257b7f5149d02a3402a
Kamil <kamil.niezrecki@gmail.com>
2026-09-21 07:30:22 +0000

    Fix build

--- app/src/main/java/kniezrec/com/flightinfo/ui/gnss/FlightParametersCard.kt
+++ app/src/main/java/kniezrec/com/flightinfo/ui/gnss/FlightParametersCard.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.LiveRegionMode
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.liveRegion
