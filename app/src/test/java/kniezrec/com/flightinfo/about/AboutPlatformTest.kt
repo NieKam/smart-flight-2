d90badf46b99eeeb96c540ed53ac4e64951c51b3
Kamil <kamil.niezrecki@gmail.com>
2026-09-22 08:09:20 +0000

    Run about platform test with Robolectric

--- app/src/test/java/kniezrec/com/flightinfo/about/AboutPlatformTest.kt
+++ app/src/test/java/kniezrec/com/flightinfo/about/AboutPlatformTest.kt
@@ -3,7 +3,12 @@ package kniezrec.com.flightinfo.about
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
 class AboutPlatformTest {
     @Test
     fun formatsCompleteVersionMetadata() {
